0d041e9d61dfe3c78598050a073791264fad5bac
amparo <114286711+amparoamparo@users.noreply.github.com>
2023-07-08 18:01:21 +0100

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    colors: {
+      "primary-bright-orange": "hsl(31, 77%, 52%)",
+      "primary-dark-cyan": "hsl(184, 100%, 22%)",
+      "primary-darker-cyan": "hsl(179, 100%, 13%)",
+      "neutral-transparent-white": "hsla(0, 0%, 100%, 0.75)",
+      "neutral-light-grey": "hsl(0, 0%, 95%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
